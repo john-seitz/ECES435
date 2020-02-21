485d5894d2b12da2e108ea4531018ad4932d7bdf
john-seitz <61063966+john-seitz@users.noreply.github.com>
2020-02-21 11:22:39 -0500

    edits

--- Assingment 3/get_bitplane.m
+++ Assingment 3/get_bitplane.m
@@ -1,5 +1,5 @@
 function [Bitplane_img] = get_bitplane(img,bitplane)
-%This function outputs the specified bitplane
+%This function outputs the specified bitplane of the input img
 img = double(img); %Convert unit8 image to double
 Bitplane_img = bitget(img,bitplane); %Extract specified bit plane from img
 end
