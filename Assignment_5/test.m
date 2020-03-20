47bfb907786a62f976a088b52e6efc76513f22b0
john-seitz <61063966+john-seitz@users.noreply.github.com>
2020-03-20 16:14:55 -0400

    changes

--- Assignment_5/test.m
+++ Assignment_5/test.m
@@ -8,47 +8,40 @@ for k = 1:length(P2Imgs)
     
     img = imread(P2Imgs{k});
     figure(k);
-    subplot(1,3,1);
+    subplot(2,3,1);
     imshow(img);
     title('Original Image PVH')
     
-    subplot(1,3,2);
-    newimg = gammacorrect(0.7,P2Imgs{k});
+    subplot(2,3,2);
+    newimg = gammacorrect(0.7,img);
     newimg = uint8(newimg);
     imshow(newimg)
     title 'Gamma = 0.7 PVH'
     
-    subplot(1,3,3);
-    newimg = gammacorrect(1.3,P2Imgs{k});
+    subplot(2,3,3);
+    newimg = gammacorrect(1.3,img);
+    newimg = uint8(newimg);
     imshow(newimg)
     title 'Gamma = 1.3 PVH'
     
+    img = imread(P2Imgs{k});
+    figure(k);
+    subplot(2,3,4);
+    imhist(img);
+    title('Original Image PVH')
+    
+    subplot(2,3,5);
+    newimg = gammacorrect(0.7,img);
+    newimg = uint8(newimg);
+    imhist(newimg)
+    title 'Gamma = 0.7 PVH'
+    
+    subplot(2,3,6);
+    newimg = gammacorrect(1.3,img);
+    newimg = uint8(newimg);
+    imhist(newimg)
+    title 'Gamma = 1.3 PVH'
+    
 end
-%% 
-P2Imgs = {'unaltIm1.tif','unaltIm2.tif','unaltIm3.tif'};
-testimg = imread(P2Imgs{3});
 
-figure (1);
-imshow(testimg)
-title 'Original'
-
-figure (2);
-gamma = 0.7;
-img2 = gammacorrect(gamma,testimg);
-img3 = uint8(img2);
-imshow(img2)
-title 'Gamma = 0.7'
 %%
-
-pout = imread('unaltIm1.tif');
-
-figure(1);
-imshow(pout)
-title 'Original'
-
-figure(2);
-gamma = 0.7; %user specified gamma value
-Newimg = gammacorrect(gamma,pout); %gamma correction function
-newpout = uint8(Newimg); %change corrected image to unit8 type
-imshow(newpout)
-title 'Gamma = 1'
